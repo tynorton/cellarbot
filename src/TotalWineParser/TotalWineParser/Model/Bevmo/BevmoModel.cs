77c3769a5594763991901f7a333915228a210bea
Ty Norton <tynorton@gmail.com>
2013-09-24 00:58:00 -0700

    Work to support bevmo

---
+++ src/TotalWineParser/TotalWineParser/Model/Bevmo/BevmoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalWineParser.Model.Bevmo
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brewery { get; set; }
+        public bool IsInStock { get; set; }
+        public string Price { get; set; }
+        public Uri ThumbUri { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
--- src/TotalWineParser/TotalWineParser/Program.cs
+++ src/TotalWineParser/TotalWineParser/Program.cs
@@ -1,36 +1,72 @@
-﻿using Newtonsoft.Json;
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TotalWineParser.Model.Bevmo;
 using TotalWineParser.Model.TotalWine;
 
 namespace TotalWineParser
 {
     class Program
     {
+        public const string IOS_USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5";
+
+        static CookieContainer s_cookieContainer = new CookieContainer();
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient();
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = s_cookieContainer,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+
+            HttpClient client = new HttpClient(handler);
+            client.DefaultRequestHeaders.Add("user-agent", IOS_USER_AGENT);
 
             var result = client.GetStringAsync("https://m.totalwine.com/json/product/list?id=%2Feng%2Fcategories%2Fbeer").Result;
 
             var obj = JsonConvert.DeserializeObject<RootObject>(result);
-
-
-            JsonTextReader reader = new JsonTextReader(new StringReader(result));
-            while (reader.Read())
+            foreach (var beer in obj.resultset.Products)
             {
-                if (reader.Value != null)
-                    Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
-                else
-                    Console.WriteLine("Token: {0}", reader.TokenType);
+                Console.WriteLine(beer.Name);
+            }
+
+            // Bellevue Store
+            //fullfilmentMethod	1
+            //fullfilmentLocation	171
+            client.GetAsync("http://m.bevmo.com/Misc/FulfillmentSelectDropDown.aspx?fullfilmentMethod=1&fullfilmentLocation=171&_=" + GetEpocTime() + "000");
+
+            var beers = new List<Beer>();
+            var resultHtml = client.GetStringAsync("http://m.bevmo.com/Shop/ProductList.aspx/Beer/Craft-Brew/_/N-15Z1z13tvr?DNID=Beer").Result;
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(resultHtml); // or Load
+            var nodes = doc.DocumentNode.SelectNodes("//div[contains(@class, 'ProductListItemTable')]");
+            foreach (var node in nodes)
+            {
+                var productName = node.SelectSingleNode(".//a[contains(@id, 'uxProductLink')]").InnerText;
+                var imgUri = node.SelectSingleNode(".//img[contains(@id, 'uxProductIconImage')]").Attributes["data-src"].Value.Replace("../../../../..", "http://m.bevmo.com");
+
+                beers.Add(new Beer { Name = productName, ThumbUri = new Uri(imgUri) });
+                Console.WriteLine(productName);
             }
 
             Console.Read();
+        }
+
+
+        private static string GetEpocTime()
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+            TimeSpan span = (DateTime.UtcNow.ToLocalTime() - epoch);
+            return span.TotalSeconds.ToString().Split('.')[0];
         }
     }
 }
