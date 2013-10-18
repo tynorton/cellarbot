c6b4a91df64a9409e2d83ca0b95fb18de5909cac
Ty Norton <github@bsd.bz>
2013-10-18 14:23:11 -0700

    Add bare bones CellarBotHome site

---
+++ src/web/CellarBotHome/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CellarBotHome.MvcApplication" Language="C#" %>
---
+++ src/web/CellarBotHome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CellarBotHome.Startup))]
+namespace CellarBotHome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
